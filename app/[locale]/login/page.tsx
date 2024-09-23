80bdbc6d834a8689710bba209413ae3f5765053e
Vinod Pittampalli <vinod@pittampalli.com>
2024-09-23 17:04:01 +0000

    adjusted docker env

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -19,7 +19,34 @@ export default async function Login({
 }: {
   searchParams: { message: string }
 }) {
+  const cookieStore = cookies()
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        }
+      }
+    }
+  )
+  const session = (await supabase.auth.getSession()).data.session
 
+  if (session) {
+    const { data: homeWorkspace, error } = await supabase
+      .from("workspaces")
+      .select("*")
+      .eq("user_id", session.user.id)
+      .eq("is_home", true)
+      .single()
+
+    if (!homeWorkspace) {
+      throw new Error(error.message)
+    }
+
+    return redirect(`/${homeWorkspace.id}/chat`)
+  }
 
   const signIn = async (formData: FormData) => {
     "use server"
